d072a8aa2add03730a30b5b2c7e77e05664db1fa
humbertovacu <humbertovacu@gmail.com>
2022-10-26 18:43:52 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+const e = require("express");
+
 class SortedList {
-  constructor() {
-    this.items = [];
-    this.length = this.items;
+  constructor(...items) {
+    this.items = items;
+    this.length = items.length;
   }
 
   add(item) {
     this.items.push(item);
     this.items.sort(function(a,b){return a - b});
+    this.length = this.items.length;
   }
 
   get(pos) {
@@ -14,23 +17,45 @@ class SortedList {
     else return this.items[pos]; 
   }
 
-  max() {}
+  max() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    else return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    else return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length) return this.items.reduce((previousValue,currentValue) =>
+      previousValue + currentValue);
+    else return 0;
+  }
 
-  avg() {}
+  avg() {
+    let totalSum = this.items.reduce((previousValue,currentValue) =>
+      previousValue + currentValue);
+    if(!this.items.length) return 0;
+    else return totalSum/this.items.length;
+  }
 }
 
 module.exports = SortedList;
 
-const testList = new SortedList(5,11,23,1);
+const testList = new SortedList();
 
 
-testList.add(7);
-testList.add(9);
-testList.add(2);
-testList.add(40);
+
+// testList.add(7);
+// testList.add(9);
+// testList.add(2);
+// testList.add(40);
+// testList.add(400);
+// testList.add(14);
+
+
+console.log(testList.length);
+
 
 
